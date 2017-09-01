1a0d42b052b6bf404411a6999d1780949db43d6b
MarcosRobertt <ro.berte@hotmail.com>
2017-08-31 21:34:09 -0300

    contador

---
+++ ConsoleApplication2/ConsoleApplication3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Contador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int cont = 1; cont <= 99; cont++)
+            {
+                Console.Write("Iniciando contador de 1 a 99!");
+                Console.Write("\nContando: " + cont);
+                Thread.Sleep(1000);
+
+                if (cont != 99)
+                {
+                    Console.Clear();
+                }
+                else
+                {
+                    Console.Write("\nFim da Contagem, pressione qualquer tecla para encerrar.");
+                }
+
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
